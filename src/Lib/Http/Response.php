e1999f5e17934d4743b94c88c15d71f78769ce8d
G <gaoya@21cake.com>
2015-09-10 17:09:27 +0800

    修改Response::json的header

--- src/Lib/Http/Response.php
+++ src/Lib/Http/Response.php
@@ -50,6 +50,7 @@ class Response
      */
     public static function json($data, $http_code = 200)
     {
+        header("Content-Type:application/json");
         http_response_code($http_code);
         echo json_encode($data);
     }
